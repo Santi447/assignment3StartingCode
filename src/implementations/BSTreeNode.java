4e29dec0d263cb94bd5d0a7439112d4d2f4ff71d
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-15 22:01:54 -0600

    BSTreeNode Implemented

--- src/implementations/BSTreeNode.java
+++ src/implementations/BSTreeNode.java
@@ -12,6 +12,7 @@ public class BSTreeNode<E>
 		this.left = null;
 		this.right = null;
 	}
+	
 	public BSTreeNode(E element, BSTreeNode<E>left,BSTreeNode<E>right) {
 		
 		this.element = element;
@@ -22,33 +23,48 @@ public class BSTreeNode<E>
 	public E getElement() {
 		return this.element;
 	}
-	public E setElement(E elementToSet) {
-		return this.element = elementToSet;
+	
+	public void setElement(E elementToSet) {
+		this.element = elementToSet;
 	}
+	
 	public BSTreeNode<E> getLeft(){
 		return this.left;
 	}
+	
+	public void setLeft(BSTreeNode<E> leftToSet){
+		this.left = leftToSet;
+	}
+	
 	public BSTreeNode<E> getRight(){
 		return this.right;
 	}
+	
+	public void setRight(BSTreeNode<E> rightToSet){
+		this.right = rightToSet;
+	}
+	
 	public boolean hasLeftChild() {
 		if(this.left == null){
 			return false;
 		}
 		return true;
 	}
+	
 	public boolean hasRightChild() {
 		if(this.right == null){
 			return false;
 		}
 		return true;
 	}
+	
 	public boolean isLeaf() {
 		if(this.left == null && this.right == null){
 			return true;
 		}
 		return false;
 	}
+	
 	public int getHeight(){
 		int leftHeight;
 		int rightHeight;
@@ -65,6 +81,24 @@ public class BSTreeNode<E>
 			rightHeight = this.right.getHeight();
 		}
 		return 1 + Math.max(leftHeight, rightHeight);
+	}
+	
+	public int getNumberNodes() {
+		int leftCount;
+		int rightCount;
+		if(this.left == null) {
+			leftCount = 0;
+		}
+		else {
+			leftCount = this.left.getNumberNodes();
+		}
+		if(this.right == null) {
+			rightCount = 0;
+		}
+		else {
+			rightCount = this.right.getNumberNodes();
+		}
+		return 1 + (leftCount + rightCount);
 	}
 	
 }
