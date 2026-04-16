8fc39a5270ef19ea22227067757ec3d8cca6298d
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-15 21:20:09 -0600

    created constructor for bs tree node

--- src/implementations/BSTreeNode.java
+++ src/implementations/BSTreeNode.java
@@ -2,5 +2,20 @@ package implementations;
 
 public class BSTreeNode<E>
 {
-
+	private E element;
+	private BSTreeNode<E> left;
+	private BSTreeNode<E> right;
+	
+	public BSTreeNode(E element) {
+		
+		this.element = element;
+		this.left = null;
+		this.right = null;
+	}
+	public BSTreeNode(E element, BSTreeNode<E>left,BSTreeNode<E>right) {
+		
+		this.element = element;
+		this.left = left;
+		this.right = right;
+	}
 }
