460bd23bf513c297f6e4a60f288717382b8c1f87
Santiago pabon <santiago.pabon72@gmail.com>
2026-04-16 10:20:18 -0600

    person 1 methods implemented

--- src/implementations/BSTree.java
+++ src/implementations/BSTree.java
@@ -1,6 +1,95 @@
 package implementations;
 
-public class BSTree<E extends Comparable<? super E>>
+import utilities.BSTreeADT;
+import utilities.Iterator;
+
+public class BSTree<E extends Comparable<? super E>> implements BSTreeADT<E>
 {
+	private BSTreeNode<E> root;
+	
+	public BSTree() {
+		root = null;
+	}
+	public BSTree(E element) {
+		this.root = new BSTreeNode<E>(element);
+	}
+	
+	@Override
+	public BSTreeNode<E> getRoot() throws NullPointerException { 
+		
+		if (root == null) {
+			throw new NullPointerException();
+			
+		}
+		return this.root ;
+	}
+
+	@Override
+	public int getHeight() {
+		return (root == null) ? 0 : root.getHeight();
+	}
+
+	@Override
+	public int size() {
+		return (root == null) ? 0 : root.getNumberNodes();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	@Override
+	public void clear() {
+		this.root = null;
+	}
+
+	@Override
+	public boolean contains(E entry) throws NullPointerException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BSTreeNode<E> search(E entry) throws NullPointerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(E newEntry) throws NullPointerException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BSTreeNode<E> removeMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BSTreeNode<E> removeMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> inorderIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> preorderIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<E> postorderIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
